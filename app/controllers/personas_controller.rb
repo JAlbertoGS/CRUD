dccbafea0133ad3725ddb3453c340caed9574565
Jose Alberto <jgomez@quaxar.com>
2021-11-11 20:24:37 -0600

    Creacion metodos create, read ,update

--- app/controllers/personas_controller.rb
+++ app/controllers/personas_controller.rb
@@ -1,7 +1,46 @@
 class PersonasController < ApplicationController
   def index
+    @personas = Persona.all.order("created_at DESC")
   end
 
   def new
+    @boton = "Registrar"
+    @persona = Persona.new
+  end
+
+  def create
+    @persona=Persona.new(persona_params)
+    if @persona.save
+      redirect_to @persona
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @boton = "Editar"
+    @persona = Persona.find(params[:id])
+  end
+
+  def update
+    @persona = Persona.find(params[:id])
+    if @persona.update(persona_params)
+      flash[:notice] = "Perfil modificado correctamente"
+      redirect_to @persona
+    else
+      render 'edit'
+    end
+  end
+
+  def show
+    @persona = Persona.find(params[:id])
+  end
+
+  def destroy
+  end
+
+
+  private def persona_params
+    params.require(:persona).permit(:name, :last_name, :email, :password)
   end
 end
--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,2 +1,6 @@
 class Persona < ApplicationRecord
+    validates :name, presence: { :message => "No puede estar vacio"}
+    validates :last_name, presence: { :message => "No puede estar vacio"}
+    validates :email, presence: { :message => "No puede estar vacio"}
+    validates :password, presence: { :message => "No puede estar vacio"}, length: {minimum: 8, :message => "Es demaciado corta, minimo 8 caracteres"}
 end
